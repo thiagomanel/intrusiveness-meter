d8346bd3fa5942d6ce4575e81f194ec8b477d5d4
armstrongmsg <goes.armstrong151@gmail.com>
2013-03-25 01:15:37 -0300

    - Started to code the whole system data collector.

--- IntrusivenessMeter/data_collector_2/system_resource_usage_collector.sh
+++ IntrusivenessMeter/data_collector_2/system_resource_usage_collector.sh
@@ -1,12 +1,36 @@
-#!/bin/bash
+#!/bin/bash -e
 
-NUMBER_OF_PROCESSORS=$1
+#
+# Federal University of Campina Grande
+# Distributed Systems Laboratory
+#
+# Author: Armstrong Mardilson da Silva Goes
+# Contact: armstrongmsg@lsd.ufcg.edu.br
+#
 
-LIMIT=`expr $NUMBER_OF_PROCESSORS - 1`
+CPU_IDLE=0
+MEMORY_USED=0
 
-for (( c=0; c<$NUMBER_OF_PROCESSORS; c++ )); do
-	MPSTAT_INFO="`mpstat -P $c | sed 1,3d`"
-	echo $MPSTAT_INFO
-done
+#
+# TODO code that may be used to get specific cpu data
+#
+#NUMBER_OF_PROCESSORS=$1
 
+#LIMIT=`expr $NUMBER_OF_PROCESSORS - 1`
 
+#for (( c=0; c<$NUMBER_OF_PROCESSORS; c++ )); do
+#	MPSTAT_INFO="`mpstat -P $c | sed 1,3d`"
+#	echo $MPSTAT_INFO
+#done
+
+function get_system_data
+{
+	sar_data=`sar -u -r 1 1 | grep "Average" | sed 1d | sed 2d`
+	CPU_IDLE=`echo $sar_data | awk '{ print $8 }'`
+	MEMORY_USED=`echo $sar_data | awk '{ print $12 }'`
+}
+
+get_system_data
+
+echo $CPU_IDLE
+echo $MEMORY_USED
