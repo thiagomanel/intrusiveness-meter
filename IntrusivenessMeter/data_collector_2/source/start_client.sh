592d3aa3f031cc2ae774d20d44e56d7282026a45
armstrongmsg <goes.armstrong151@gmail.com>
2013-04-30 01:45:51 -0300

    Task #2149 Write scripts to start and stop tool
    - Added scripts to both start and stop

--- IntrusivenessMeter/data_collector_2/source/start_client.sh
+++ IntrusivenessMeter/data_collector_2/source/start_client.sh
@@ -1,13 +1,34 @@
-#!/bin/bash
+#!/bin/bash -e
 
+#
+# Federal University of Campina Grande
+# Distributed Systems Laboratory
+#
+# Author: Armstrong Mardilson da Silva Goes
+# Contact: armstrongmsg@lsd.ufcg.edu.br
+#
+
+#
+# Intrusiveness Meter Client Starter
+#
+# This script starts all the programs of the Intrusiveness Meter Client
+#
+
+# FIXME it should be read from a conf file
 RESULTS_DIRECTORY="../results"
 
 SYSTEM_RESOURCE_COLLECTOR="system_resource_usage_collector.sh"
+# FIXME it should be read from a conf file
 DEVICE_TO_MONITOR="/dev/sda7"
+# FIXME it should be read from a conf file
 SYSTEM_RESOURCE_BASE_OUTPUT_FILENAME="monitoring"
 
 HADOOP_AWARE_COLLECTOR="hadoop_aware_collector.py"
 
+TASK_TRACKER_DAEMON="task_tracker_start_daemon.sh"
+
 ./$SYSTEM_RESOURCE_COLLECTOR $SYSTEM_RESOURCE_BASE_OUTPUT_FILENAME $RESULTS_DIRECTORY $DEVICE_TO_MONITOR &
 
 python $HADOOP_AWARE_COLLECTOR $RESULTS_DIRECTORY &
+
+./$TASK_TRACKER_DAEMON &
---
+++ IntrusivenessMeter/data_collector_2/source/stop_client.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -e
+
+#
+# Federal University of Campina Grande
+# Distributed Systems Laboratory
+#
+# Author: Armstrong Mardilson da Silva Goes
+# Contact: armstrongmsg@lsd.ufcg.edu.br
+#
+
+#
+# Intrusiveness Meter Client Stopper
+#
+# This script stops all the programs of the Intrusiveness Meter Client
+#
+
+function stop_system_resource_collector
+{
+	PID="`ps xau | grep system_resource_usage_collector.sh | awk '{ print $2}'`"
+	kill $PID
+}
+
+function stop_hadoop_aware_collector
+{
+	PID="`ps xau | grep hadoop_aware_collector.py | awk '{ print $2}'`"
+	kill $PID
+}
+
+function stop_task_tracker_daemon
+{
+	PID="`ps xau | grep task_tracker_start_daemon.sh | awk '{ print $2}'`"
+	kill $PID
+}
+
+stop_system_resource_collector
+stop_hadoop_aware_collector
+stop_task_tracker_daemon
